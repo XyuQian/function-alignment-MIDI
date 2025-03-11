633d07533e921620ff49d3af56ebebe2d6129bd3
anonymous-16 <linliwei3916@gmail.com>
2025-03-12 00:52:15 +0400

    update

--- scripts/finetune_midi_2_audio.sh
+++ scripts/finetune_midi_2_audio.sh
@@ -5,7 +5,7 @@ e=30
 w=1
 exp_dir=midi_2_audio
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m shoelace.shoelace.midi_2_audio.finetune \
+CUDA_VISIBLE_DEVICES=0 python -m shoelace.shoelace.midi_2_audio.finetune \
       --learning_rate=$lr \
       --experiment_folder=$exp \
       --batch_size=$batch_size \
